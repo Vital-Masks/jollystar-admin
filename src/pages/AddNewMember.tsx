8c52fec1715131470ecf2da545fdd5214d1db79d
Vetha <vitalmasks@gmail.com>
2024-12-21 17:23:15 +0530

    add new members

--- src/pages/AddNewMember.tsx
+++ src/pages/AddNewMember.tsx
@@ -55,7 +55,7 @@ interface ClubDetail {
 }
 
 interface PaymentDetails {
-    paymentCategory: string;
+    memberType: string;
     memberId: string;
     bank: string;
     branch: string;
@@ -525,7 +525,7 @@ const AddNewMember = () => {
             total: formValues.pdtotal,
             date: formValues.pddate,
             paymentSlip: formValues.pdpaymentImage,
-            paymentCategory: formValues.pdcategory,
+            memberType: formValues.pdcategory,
         };
 
         if (editIndex !== null) {
